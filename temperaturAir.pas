882b64c57d7c25acbb3b4e4ed43081715fb55088
Muhammad Alvaro Khikman <114148484+muhammadalvarokhikman@users.noreply.github.com>
2022-11-04 12:34:46 +0700

    Add files via upload
    Hello guys, you can download this file , have a good day xixixi

---
+++ temperaturAir.pas
@@ -0,0 +1,19 @@
+program temperaturAir;
+uses crt;
+var
+        T:byte;
+begin
+clrscr;
+        writeln('Selamat datang di program TEMPERATUR AIR(T)');
+        writeln('Author : Muhammad Alvaro Khikman (B2A022061)');
+        write('Silahkan masukan berapa suhu air = ');read(T);
+        if(T<=0) then
+                writeln('Air berwujud padat')
+        else if(T >0)and(T<100) then
+                writeln('Air berwujud cair')
+        else
+                writeln('Air berwujud uap');
+        writeln();
+readln;
+readln;
+end.
