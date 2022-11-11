cfc2f4e3bcb7fa814506cac2b3214390b5a8815c
Muhammad Alvaro Khikman <114148484+muhammadalvarokhikman@users.noreply.github.com>
2022-11-11 10:09:54 +0700

    Add files via upload

---
+++ nilai.pas
@@ -0,0 +1,32 @@
+program rentangNilai;
+uses crt;
+var
+nilai:real;
+begin
+clrscr;
+        writeln('====================================================');
+        writeln('Selamat Datang Di Program Rentang Nilai');
+        writeln('Nama = Muhammad Alvaro Khikman');
+        writeln('NIM  = B2A022061');
+        writeln('====================================================');
+
+        write('Masukkan Nilai mahasiswa = ');read(nilai);
+        if((nilai>=80)and(nilai<=100))then
+                writeln('Nilai mahasiswa A')
+        else if((nilai>=75)and(nilai<=79.99)) then
+                writeln('Nilai mahasiswa AB')
+        else if((nilai>=70)and(nilai<=74.99)) then
+                writeln('Nilai mahasiswa B')
+        else if((nilai>=65)and(nilai<=69.99)) then
+                writeln('Nilai mahasiswa BC')
+        else if((nilai>=60)and(nilai<=64.99)) then
+                writeln('Nilai mahasiswa C')
+        else if((nilai>=50)and(nilai<=59.99)) then
+                writeln('Nilai mahasiswa CD')
+        else if((nilai>=40)and(nilai<=49.99)) then
+                writeln('Nilai mahasiswa D')
+        else
+                writeln('Nilai mahasiswa E');
+readln;
+readln;
+end.
