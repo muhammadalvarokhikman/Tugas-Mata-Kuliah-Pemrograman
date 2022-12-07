bade13b69ebf97531a77749419c5777e2156ed93
Muhammad Alvaro Khikman <114148484+muhammadalvarokhikman@users.noreply.github.com>
2022-12-07 16:44:07 +0700

    Add files via upload

---
+++ shidqi.pas
@@ -0,0 +1,24 @@
+program Penjualan_Jajanan_Pasar;
+uses crt;
+var
+napem:string;
+nj:string;
+hrg,jb,jh,tot:real;
+
+begin
+clrscr;
+writeln('-----------------------------');
+writeln('DATA PENJUALAN JAJANAN PASAR/PCS');
+writeln('-----------------------------');
+write('Nama Pembeli = ');read(napem);
+write('Nama Jajan = ');read(nj);
+
+write(napem);
+write(nj);
+
+readln;
+readln;
+end.
+
+
+
