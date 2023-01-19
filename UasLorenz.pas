e975ccf2dc67966193535e2925cbc6d71884f1bb
Muhammad Alvaro Khikman <114148484+muhammadalvarokhikman@users.noreply.github.com>
2023-01-19 19:32:00 +0700

    Add files via upload

---
+++ UasLorenz.pas
@@ -0,0 +1,18 @@
+program LuasSegitiga;
+uses crt;
+var
+  alas,luas, tinggi: real;
+begin
+  clrscr;
+  writeln('=========================');
+  writeln('  Program Luas Segitiga  ');
+  writeln('=========================');
+  write('Masukkan nilai alas   : ');
+  readln(alas);
+  write('Masukkan nilai tinggi : ');
+  readln(tinggi);
+  luas:= 0.5*alas*tinggi;
+  writeln('Luas segitiga adalah  : ', luas:0:0);
+  writeln('=========================');
+  readln;
+end.
